c27c4bbcb67d94d76d19376de8a9597603dd3785
Seba237 <perezsebastian2509@gmail.com>
2026-06-09 19:37:01 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,39 +1,5 @@
-const CACHE_NAME = 'jefatura-bcql-v8.37';
-const ASSETS = [
-  '/jefatura-quilmes/',
-  '/jefatura-quilmes/index.html',
-  '/jefatura-quilmes/manifest.json',
-  '/jefatura-quilmes/icon-192.png',
-  '/jefatura-quilmes/icon-512.png'
-];
-
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(ASSETS);
-    })
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.filter(function(k) { return k !== CACHE_NAME; })
-            .map(function(k) { return caches.delete(k); })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(cached) {
-      return cached || fetch(e.request).catch(function() {
-        return caches.match('/jefatura-quilmes/index.html');
-      });
-    })
-  );
-});
+const CACHE_NAME = 'jefatura-bcql-v8.38';
+const urlsToCache = ['/'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
